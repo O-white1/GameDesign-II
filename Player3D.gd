be561fb6fcf6b311d0358c48b57e787f5056b2dc
O-white1 <white.o3@stu.janesville.k12.wi.us>
2024-02-12 15:26:16 -0600

    GD2 Update(2/12/24 3:26 PM)

--- Player3D.gd
+++ Player3D.gd
@@ -2,12 +2,19 @@ extends CharacterBody3D
 
 
 const SPEED = 5
-const JUMP_VELOCITY = 5
+const JUMP_VELOCITY = 10
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 const CAM_SENSITIVITY = 0.03
-@onready var camera = $Camera3D
+@onready var camera = $head/Camera3D
+@onready var camera_arm = $SpringArm3D
+@onready var camera_pos = camera.position
+var first_person = true
+
+
+@onready var model = $gobot
+@onready var animator = $gobot/AnimationPlayer
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -23,9 +30,11 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		animator.play("Walk")
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
+		animator.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
@@ -36,14 +45,43 @@ func _physics_process(delta):
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	if Input.is_action_just_pressed("Change_camera"):
+		toggle_camera_parent()
+	var rot_dir = Input.get_vector("rot_left", "rot_right", "rot_up", "rot_down").normalized()
+			
+			
+	if rot_dir:
+		self.rotation.x += rot_dir.x / 100.0
+		self.rotation.z += rot_dir.y / 100.0
 	move_and_slide()
+	
+	
+func toggle_camera_parent():
+	var parent = "head"
+	if first_person:
+		parent = "SpringArm3D"
+		model.visible = true
+	var child = camera
+	child.get_parent().remove_child(child)
+	get_node(parent).add_child(child)
+	camera = child
+	if not first_person:
+		model.visible = false
+		camera.position
+	first_person = not first_person
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	model.visible = false
 	
 	
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		self.rotate_y(-event.relative.x * (CAM_SENSITIVITY / 10.0))
-		camera.rotate_x(-event.relative.y * (CAM_SENSITIVITY / 10.0))
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		if first_person:
+			self.rotate_y(-event.relative.x * (CAM_SENSITIVITY / 10.0))
+			camera.rotate_x(-event.relative.y * (CAM_SENSITIVITY / 10.0))
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		else:
+			self.rotate_y(-event.relative.x * (CAM_SENSITIVITY / 10.0))
+			camera_arm.rotate_x(-event.relative.y * (CAM_SENSITIVITY / 10.0))
+			camera_arm.rotation.x = clamp(camera.rotation.x, deg_to_rad(-75), deg_to_rad(75))
