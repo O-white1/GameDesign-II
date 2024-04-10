94f8dc139b119032c37e7af54337b2083830be9e
O-white1 <white.o3@stu.janesville.k12.wi.us>
2024-04-10 15:27:50 -0500

    3/27 car stuff do be doin

--- rc_player.gd
+++ rc_player.gd
@@ -21,7 +21,20 @@ func _physics_process(delta):
 	var accel = Input.get_axis("ui_up", "ui_down") * HORSE_POWER
 	$backLeft.engine_force = calc_engine_force(accel, abs($backLeft.get_rpm()))
 	$backRight.engine_force = calc_engine_force(accel, abs($backRight.get_rpm()))
-	# todo: mph label
-	# todo: right vehicle
-	# todd: chase camera
+	
+	var fwd_mps = abs((self.linear_velocity * self.transform.basis).z)
+	$Label.text = "%d mph" % (fwd_mps * 2.23694) # meteres per second to miles per hour
+	
+	$CenterMass.global_position = $CenterMass.global_position.lerp(self.global_position, delta*20.0)
+	$CenterMass.transform = $CenterMass.transform.interpolate_with(self.transform, delta*5.0)
+	$CenterMass/Camera3D.look_at(self.global_position.lerp(self.global_position + self.linear_velocity, delta*5.0))
+	check_and_right_vehicle()
+	
+func check_and_right_vehicle():
+	if self.global_transform.basis.y.dot(Vector3.UP) < 0:
+		var current_rotation = self.rotation_degrees
+		current_rotation.x = 0
+		current_rotation.z = 0
+		self.rotation_degrees = current_rotation
+		
 	
