5def051df24c65499afbdc2758debf1a68879ce7
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-04-25 15:27:59 -0500

    updooter

--- rc_enemy.gd
+++ rc_enemy.gd
@@ -10,18 +10,29 @@ var STUCK_TIMER = 0
 var is_stuck = false
 
 @onready var rayF = $RayForaward
-@onready var rayFW = $RayForwLeft
+@onready var rayFL = $RayForwLeft
 @onready var rayFR = $RayForwRight
 @onready var rayL = $RayLeft
 @onready var rayR = $RayRight
 
 
 func physics_process(delta):
-	pass	
-	
-	
-	
-	
+	var target_steering = 0.0
+	update_raycasts()
+	var acceleration = HORSE_POWER
+	if rayF.is_colliding():
+		var dist_to_obstacle = rayF.get_collision_point().distance_to(global_transform.origin)
+		acceleration *= max(0.1, dist_to_obstacle / 10.0)
+		if rayFL.is_colliding or rayL.is_colliding(): target_steering -= MAX_STEER
+		if rayFR.is_colliding: target_steering += MAX_STEER
+		if is_stuck: acceleration = REVERSE_POWER
+		steering = -sin(steering) * MAX_STEER
+		
+		$backLeft.engine_force = calc_engine_force(acceleration, abs($backLeft.get_rpm()))
+		$backRight.engine_force = calc_engine_force(acceleration, abs($backRight.get_rpm()))
+		check_and_right_vehicle()
+		
+		
 func check_stuck_condition(delta):
 	if linear_velocity.length() < 1:
 		STUCK_TIMER += delta
@@ -31,7 +42,7 @@ func check_stuck_condition(delta):
 func update_raycasts():
 	rayF.force_raycast_update()
 	rayFR.force_raycast_update()
-	rayFW.force_raycast_update()
+	rayFL.force_raycast_update()
 	rayL.force_raycast_update()
 	rayR.force_raycast_update()
 
